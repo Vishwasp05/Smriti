8f4632c826a06f42b1e15ea53bb6eb640ee3ccd6
Vishwas Sharma <ch0nky@protonmail.com>
2024-04-22 22:40:29 +0530

    Tab-Bar-Base-Setup-Done

---
+++ Smriti/Views/ResourceView.swift
@@ -0,0 +1,18 @@
+//
+//  ResourceView.swift
+//  Smriti
+//
+//  Created by Vishwas Sharma on 22/04/24.
+//
+
+import SwiftUI
+
+struct ResourceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResourceView()
+}
