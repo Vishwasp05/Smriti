aa1743dd7c271d80f5e01a3e6151768c9073ab76
Vishwas Sharma <ch0nky@protonmail.com>
2024-04-22 19:38:46 +0530

    Initial Commit

---
+++ Smriti/SmritiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmritiApp.swift
+//  Smriti
+//
+//  Created by Vishwas Sharma on 22/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SmritiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
